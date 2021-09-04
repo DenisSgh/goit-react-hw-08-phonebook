d169379d8e098968085508102eb604e4d0e5d94f
Denis Singh <denis.darkrgd@gmail.com>
2021-09-04 21:11:03 +0300

    Updated main routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import Form from 'components/Form';
-import Filter from 'components/Filter';
-import Contacts from 'components/Contacts';
+import { useEffect, lazy, Suspense } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Switch } from 'react-router-dom';
+import Container from 'components/Container';
+import AppBar from 'components/AppBar';
+import PrivateRoute from 'components/PrivateRoute';
+import PublicRoute from 'components/PublicRoute';
+import { fetchCurrentUser } from 'redux/auth/authOperations';
+import { getIsFetchingCurrentUser } from 'redux/auth/authSelectors';
+
+const HomeView = lazy(() => import('./views/HomeView'));
+const RegisterView = lazy(() => import('./views/RegisterView'));
+const LoginView = lazy(() => import('./views/LoginView'));
+const ContactsView = lazy(() => import('./views/ContactsView'));
 
 export default function App() {
+  const dispatch = useDispatch();
+  const isFetchingCurrentUser = useSelector(getIsFetchingCurrentUser);
+
+  useEffect(() => {
+    dispatch(fetchCurrentUser());
+  }, [dispatch]);
+
   return (
-    <>
-      <Form />
-      <Filter />
-      <Contacts />
-    </>
+    !isFetchingCurrentUser && (
+      <Container>
+        <AppBar />
+
+        <Switch>
+          <Suspense fallback={''}>
+            <PublicRoute exact path="/">
+              <HomeView />
+            </PublicRoute>
+
+            <PublicRoute path="/register" restricted>
+              <RegisterView />
+            </PublicRoute>
+
+            <PublicRoute path="/login" redirectTo="/contacts" restricted>
+              <LoginView />
+            </PublicRoute>
+
+            <PrivateRoute path="/contacts" redirectTo="/login">
+              <ContactsView />
+            </PrivateRoute>
+          </Suspense>
+        </Switch>
+      </Container>
+    )
   );
 }
