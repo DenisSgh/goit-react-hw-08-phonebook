9339e311b18d137500a384b5e7ff2d87ec72bb49
Denis Singh <denis.darkrgd@gmail.com>
2021-09-04 21:20:58 +0300

    Added persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,41 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
-import reducer from './contacts/contactsReducer';
+// import logger from 'redux-logger';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import contactsReducer from './contacts/contactsReducer';
+import authReducer from './auth/authSlice';
 
-const middleware = [...getDefaultMiddleware(), logger];
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
-const store = configureStore({
-  reducer: { contacts: reducer },
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  // logger,
+];
+
+export const store = configureStore({
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    contacts: contactsReducer,
+  },
   middleware,
-  devTools: process.env.NODE_ENV === 'development',
+  devTools: process.env.NODE_ENV === 'development', //Allows devtools only during development
 });
 
-export default store;
+export const persistor = persistStore(store);
